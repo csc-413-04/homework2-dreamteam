8a5abc75b59d664c9cfc4b5a2cdbb9f2c035fe02
twglm9 <tachogardiner@gmail.com>
2018-10-23 16:37:08 -0700

    spark endpoints

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,42 @@
 package main.java;
+
 import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
-      // staticFiles.externalLocation("public");
-      // http://sparkjava.com/documentation
-      port(1234);
-      // calling get will make your app start listening for the GET path with the /hello endpoint
-      get("/hello", (req, res) -> "Hello World");
+        // staticFiles.externalLocation("public");
+        // http://sparkjava.com/documentation
+        port(1234);
+        // calling get will make your app start listening for the GET path with the /hello endpoint
+        get("/hello", (req, res) -> "Hello World");
+
+        ///newuser?username=<username>&password=<pass>
+        // AND /newuser?username=<anotheruser>&password=<pass>
+        get("/newuser", (req, res) -> {
+            return req.queryMap().get("username").value() + req.queryMap().get("password").value();
+        });
+
+        ///login?username=<username>&password=<wrongpassword>
+        // AND /login?username=<username>&password=<pass>
+        get("/login", (req, res) -> {
+            //req.queryMap().get("username").value();
+            //req.queryMap().get("password").value();
+            return "login_failed";
+        });
+
+        // /addfriend?token=<badtoken>&friend=<freindsuserid>
+        get("/addfriend", (req, res) -> {
+            //req.queryMap().get("token").value();
+            //req.queryMap().get("friend").value();
+            return "failed_authentication";
+        });
+
+        // /friends?token=<token>
+        get("friends", (req, res) -> {
+            //req.queryMap().get("token").value();
+            return " list of friends";
+        });
 
     }
 }
