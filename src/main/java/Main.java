6b88110307568418af9ce2ddea7ccdc53cfc2805
twglm9 <tachogardiner@gmail.com>
2018-10-23 19:04:01 -0700

    query to dbobject to collection process detailed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,14 +26,14 @@ public class Main {
         // calling get will make your app start listening for the GET path with the /hello endpoint
         get("/hello", (req, res) -> "Hello World");
 
-        ///newuser?username=<username>&password=<pass>
+        ///newuser?username=<username>&password=<pass> HANDLED
         // AND /newuser?username=<anotheruser>&password=<pass>
         get("/newuser", (req, res) -> {
-            BasicDBObject newuser = new BasicDBObject();
+            BasicDBObject newuser = new BasicDBObject();                            //build newuser to put into collection of users
             newuser.put("username", req.queryMap().get("username").value());
             newuser.put("password", req.queryMap().get("password").value());
-            colusers.insert(newuser);
-            return colusers.getCount();
+            colusers.insert(newuser);                                               //put newuser into collection of users
+            return "okay";
         });
 
         ///login?username=<username>&password=<wrongpassword>
@@ -41,6 +41,8 @@ public class Main {
         get("/login", (req, res) -> {
             //req.queryMap().get("username").value();
             //req.queryMap().get("password").value();
+            //build basicdbobject with username/password, look up in colusers, if match return login OK, if not login_failed
+            //colusers.find(); maybe
             return "login_failed";
         });
 
